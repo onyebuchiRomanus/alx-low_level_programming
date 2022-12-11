956b61611374ad9d3be682eb866767c55f4b75f6
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-11 10:09:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,10 @@
  *Description - returns the puts function
  *Return: Always 0 (Success)
  */
+
 int main(void)
 {
   puts("\"programming is like building a multilingual puzzle");
-    return(0);
+  
+  return(0);
 }
