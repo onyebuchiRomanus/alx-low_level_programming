7682fc911065ad3cb364229952edc55352dd9c3c
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-12 08:11:27 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,8 @@ float floatType;
 double doubleType;
 char charType;
 printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of floatTYpe: %zu bytes\n", sizeof(floatType));
-printf("Size of doubleType: %zu bytes\n", sizeof(doubleType));
-printf("Size of charType: %zu bytes\n", sizeof(charType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu bytes\n", sizeof(charType));
 return (0)
 }
