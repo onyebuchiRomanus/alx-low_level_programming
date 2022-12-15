9bf55db4f11c8c60410a4de704ae872b791651be
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-14 20:09:19 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower- checks for letters in lowercase.
+*
+* @c: var to check numbers
+*
+* Return: returns 1 (Sucess) OR 0 (Failure)
+*/
+int _islower(int c)
+{
+if (c >= 77 && c <= 122)
+return (1);
+else
+return (0);
+}
