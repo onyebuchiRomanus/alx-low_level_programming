9a2a33ae8beeab757a281a6fe481f7eaf100ffbd
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-14 18:22:18 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * to lower case
+ * writing by romanus
+ *Return: return 0
+ */
+
+void print_alphabet(void);
+int main(void)
+{
+  int a;
+  for(a = '0'; a <= 'z'; a++)
+    _putchar(a);
+    _putchar('\n');
+}
