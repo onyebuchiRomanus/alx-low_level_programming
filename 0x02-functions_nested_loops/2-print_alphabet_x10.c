7903eea9343ebe498e20c597c2cc8c04a1579dcd
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-14 19:24:52 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10- prints 10 times the alphabet
+ *
+ *_putchar: prints to standard output
+ */
+void print_alphabet_x10(void)
+{
+  char a;
+  int b;
+  
+  for (a = 0; a < 10; a++)
+    {
+      for (b = 'a'; b < 'z'; b++)
+	{
+	  _putchar(b);
+	  
+  
+	}
+      _putchar('\n');
+    }
+}
