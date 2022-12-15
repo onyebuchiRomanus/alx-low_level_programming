99e9f9563d97ea6c24d525c9ad96ea0d75b78ced
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-15 05:31:41 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <ctype.h>
 /**
- *_isalpha - checks for alphabetic character
- *isalpha - checks for c
- *@c: checks alphabetic
- *Return 1 (success) or 0 (failure)
- */
+*_isalpha - checks for alphabetic character.
+*
+*islapha - checks for c var.
+*@c: checks alphabetic
+*Return 1 (success) or 0 (failure)
+*/
 int _isalpha(int c)
 {
 if (isalpha(c))
