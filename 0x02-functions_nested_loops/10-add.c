d225f45d21dd6a79ef9b1ff233960a678935544e
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-16 06:41:42 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/**
+ *program: A function that adds two integers and returns the result
+ *Autho:Onyebuchi Romanus Ogbonna.
+ */
+#include "main.h"
+/**
+ *add- adds integers and returns the result.
+ *@a:first number
+ *@b:sencond number
+ *
+ *Return: returns the result of the function add.
+ */
+int add(int a, int b)
+{
+  int result;
+  result = a + b;
+  return (result);
+}
