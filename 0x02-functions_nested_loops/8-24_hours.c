b3414312128d0a749e93ae556f843497702c290c
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-15 18:51:58 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *void jack_bauer- prints every minutes of the day of jack-bauer stating from 00:00 to 23:59
+ *void jack_bauer- prints every minutes of the day of jack-bauer 
+ *stating from 00:00 to 23:59
  *
  *Return: return always 0.
  *_putchar- prints standard output
@@ -12,11 +13,11 @@ for (hour = 0; hour <= 23; hour++)
 {
 for (minute = 0; minute <= 59; minute++)
 {
-_putchar(hour/10 + '0');
-_putchar(hour%10 + '0');
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
 _putchar(':');
-_putchar(minute/10 + '0');
-_putchar(minute%10 + '0');
+_putchar(minute / 10 + '0');
+_putchar(minute % 10 + '0');
 _putchar('\n');
 }
 }
