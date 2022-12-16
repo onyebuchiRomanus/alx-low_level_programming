c58c1bb111a16918ea50c7be971eb9c3a971c332
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-16 06:02:06 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,22 +2,21 @@
 /**
  * times_table - Prints the 9 times table, starting with 0.
  */
-void times_table(void)
 {
-int num, mult, result;
+int num, mult, res;
 for (num = 0; num <= 9; num++)
-_putchar('0');
 {
+_putchar('0');
 for (mult = 1; mult <= 9; mult++)
 {
 _putchar(',');
 _putchar(' ');
-result = num * mult;
-if (result <= 9)
+res = num * mult;
+if (res <= 9)
 _putchar(' ');
 else
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
 }
 _putchar('\n');
 }
