123e6ccf9df5a694bcdb691265a7d794135e919f
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-20 11:29:20 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,7 +8,7 @@ int main(void)
 {
 char s1[98];
 char *ptr;
-ptr = strcpy(s1, "first,solve the problem. then, write the code\n");
+ptr = _strcpy(s1, "first,solve the problem. then, write the code\n");
 printf("%s", s1);
 printf("%s", ptr);
 return (0);
