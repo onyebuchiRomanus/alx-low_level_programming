008e4b5f73139329ae92144fa1f358f7d2ef41eb
onyebuchiRomanus <romanusonyebuchi99@gmail.com>
2022-12-20 07:53:51 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "main.j"
+#include "main.h"
 /**
  *_puts - prints a string followed by a new line
  *@str : string to print
@@ -7,7 +7,7 @@
 void _puts(char *str)
 {
 int = 0;
-while(str[a] != '\0')
+while (str[a] != '\0')
 _putchar(str[a]);
 a++;
 }
